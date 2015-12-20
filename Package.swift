b627563b680d439ec47b45357ddb3d2ccf3fdbdd
Paulo Faria <paulo.faria.rl@gmail.com>
2015-12-20 06:47:21 -0800

    fix toolchain 2015-12-18 SPM bug

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
 	name: "OpenSSL",
 	dependencies: [
 		.Package(url: "https://github.com/Zewo/COpenSSL.git", majorVersion: 0, minor: 1),
-		.Package(url: "https://github.com/Zewo/Core.git", majorVersion: 0, minor: 1)
+		.Package(url: "https://github.com/Zewo/Core.git", majorVersion: 0, minor: 1),
+		.Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 1)
 	]
 )
