701314c822cfd31bc360b2783c63a3270a53817f
Paulo Faria <paulo.faria.rl@gmail.com>
2015-12-20 17:03:31 -0200

    support OSX

--- Package.swift
+++ Package.swift
@@ -1,9 +1,15 @@
 import PackageDescription
 
+#if os(OSX)
+    let openSSLURL = "https://github.com/Zewo/COpenSSL-OSX.git"
+#else
+    let openSSLURL = "https://github.com/Zewo/COpenSSL.git"
+#endif
+
 let package = Package(
 	name: "OpenSSL",
 	dependencies: [
-		.Package(url: "https://github.com/Zewo/COpenSSL.git", majorVersion: 0, minor: 1),
+		.Package(url: openSSLURL, majorVersion: 0, minor: 1),
 		.Package(url: "https://github.com/Zewo/Core.git", majorVersion: 0, minor: 1),
 		.Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 1)
 	]
