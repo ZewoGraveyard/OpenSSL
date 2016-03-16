88fa377996fed4f7ad8e0e710b5faa231561a541
antonmes <antonmes@users.noreply.github.com>
2016-03-16 04:10:08 +0300

    Add COpenSSL

--- Package.swift
+++ Package.swift
@@ -1,18 +1,11 @@
 import PackageDescription
 
-#if os(OSX)
-    let openSSLURL = "https://github.com/Zewo/COpenSSL-OSX.git"
-#else
-    let openSSLURL = "https://github.com/Zewo/COpenSSL.git"
-#endif
-
 let package = Package(
     name: "OpenSSL",
     dependencies: [
-        .Package(url: openSSLURL, majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/Stream.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/File.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/Venice.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/CLibvenice.git", majorVersion: 0, minor: 2)
+        .Package(url: "https://github.com/Zewo/Stream.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/Zewo/File.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/Zewo/Venice.git", majorVersion: 0, minor: 4),
     ]
 )
+
