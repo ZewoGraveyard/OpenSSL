7fe0bace70d78532a81476eeca4df8d97787bb58
antonmes <antonmes@users.noreply.github.com>
2016-03-18 23:09:47 +0300

    Restore COpenSSL dependency

--- Package.swift
+++ Package.swift
@@ -2,11 +2,11 @@ import PackageDescription
 
 let package = Package(
     name: "OpenSSL",
-    targets: [Target(name: "OpenSSL", dependencies: ["COpenSSL"])],
     dependencies: [
         .Package(url: "https://github.com/Zewo/Stream.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/Zewo/File.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/Zewo/Venice.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/Zewo/COpenSSL.git", majorVersion: 0, minor: 4),
     ]
 )
 
