8c9475274d928fe11752e60868aa6cf5eb30a01d
antonmes <antonmes@users.noreply.github.com>
2016-03-18 22:57:28 +0300

    Fix C dependency

--- Package.swift
+++ Package.swift
@@ -2,6 +2,7 @@ import PackageDescription
 
 let package = Package(
     name: "OpenSSL",
+    targets: [Target(name: "OpenSSL", dependencies: ["COpenSSL"])],
     dependencies: [
         .Package(url: "https://github.com/Zewo/Stream.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/Zewo/File.git", majorVersion: 0, minor: 4),
