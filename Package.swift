cbceee9a2fedf50adefc0b4b936e80444ee3e828
antonmes <antonmes@users.noreply.github.com>
2016-03-19 00:38:58 +0300

    Remove OSX rependency

--- Package.swift
+++ Package.swift
@@ -6,11 +6,6 @@ let package = Package(
         .Package(url: "https://github.com/Zewo/Stream.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/Zewo/File.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/Zewo/Venice.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/Zewo/COpenSSL.git", majorVersion: 0, minor: 4),
     ]
 )
-
-#if os(OSX)
-package.dependencies.append(.Package(url: "https://github.com/Zewo/COpenSSL-OSX.git", majorVersion: 0, minor: 4))
-#else
-package.dependencies.append(.Package(url: "https://github.com/Zewo/COpenSSL.git", majorVersion: 0, minor: 4))
-#endif
