f9c51c225f0e08118508648c47e0c3f350c2b590
Paulo Faria <paulo.faria.rl@gmail.com>
2016-03-27 16:39:56 -0300

    add MIT header

--- Package.swift
+++ Package.swift
@@ -1,4 +1,34 @@
+// Package.swift
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Zewo
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 import PackageDescription
+
+#if os(OSX)
+    let COpenSSLURL = "https://github.com/Zewo/COpenSSL-OSX.git"
+#else
+    let COpenSSLURL = "https://github.com/Zewo/COpenSSL.git"
+#endif
 
 let package = Package(
     name: "OpenSSL",
@@ -6,6 +36,6 @@ let package = Package(
         .Package(url: "https://github.com/Zewo/Stream.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/Zewo/File.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/Zewo/Venice.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/Zewo/COpenSSL.git", majorVersion: 0, minor: 4),
+        .Package(url: COpenSSLURL, majorVersion: 0, minor: 4),
     ]
 )
