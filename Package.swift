9b1deb80432bcb286978db525b692391c815d6d2
antonmes <antonmes@users.noreply.github.com>
2016-04-20 01:42:01 +0300

    Remove COpenSSL-OSX dependency

--- Package.swift
+++ Package.swift
@@ -1,15 +1,9 @@
 import PackageDescription
 
-#if os(OSX)
-    let COpenSSLURL = "https://github.com/Zewo/COpenSSL-OSX.git"
-#else
-    let COpenSSLURL = "https://github.com/Zewo/COpenSSL.git"
-#endif
-
 let package = Package(
     name: "OpenSSL",
     dependencies: [
         .Package(url: "https://github.com/VeniceX/File.git", majorVersion: 0, minor: 5),
-        .Package(url: COpenSSLURL, majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/Zewo/COpenSSL.git", majorVersion: 0, minor: 5),
     ]
 )
